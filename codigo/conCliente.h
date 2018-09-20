e38b09d967e92b26f659a525b6f0f8d98015e02d
meunoem <meu@email.bom>
2018-09-20 14:15:19 -0300

    controle cliente adicionado

---
+++ codigo/conCliente.h
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   conCliente.h
+ * Author: alunos
+ *
+ * Created on 20 de Setembro de 2018, 13:48
+ */
+
+#ifndef CONCLIENTE_H
+#define CONCLIENTE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "vector.h"
+
+int salvaCliente(vector nome,vector endereco,vector cpf, vector telefone, vector email, char sexo, vector estado_civil
+, vector data_nascimento){}
+
+int atualizaCliente(Cliente c){}
+
+int deletaCliente(int codCliente){}
+
+
+
+
+#endif /* CONCLIENTE_H */
+
--- codigo/controle/conCliente.c
+++ codigo/controle/conCliente.c
@@ -3,4 +3,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//#include "../clienteDAO.h"
+#include "../clienteDAO.h"
+
+int salvaCliente(vector nome,vector endereco,vector cpf, vector telefone, vector email, char sexo, vector estado_civil
+, vector data_nascimento){
+    Cliente c;
+    c.nome=nome;
+    c.cpf=cpf;
+    c.data_nascimento=data_nascimento;
+    c.email=email;
+    c.endereco=endereco;
+    c.estado_civil=estado_civil;
+    c.sexo=sexo;
+    c.telefone=telefone;
+    c.deletado=" ";
+    
+    return inclusaoCliente(c);
+}
+
+int atualizaCliente(Cliente c){
+    return alterarCliente(c,c.codigo);
+}
+
+int deletaCliente(int codigo){
+    return excluirCliente(codigo);
+}
+
+vector listaClientes(){
+    return listarClientes();
+}
+
+Cliente consultaCliente(int codigo){
+    return consultarClientes(codigo);
+}
+
+
